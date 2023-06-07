7207ba9e866a0978faab9a9950caa7e02f0f7dc9
santiagoshinder <santiagoshinder@gmail.com>
2023-06-07 16:38:06 -0300

    End first delivery

---
+++ js/speech.js
@@ -0,0 +1,18 @@
+var rec;
+    if(!("webkitSpeechRecognition" in window)) {
+        alert("Sorry, you don't use custom API");
+    } else {
+        rec = new webkitSpeechRecognition();
+        rec.lang = "es-en";
+        rec.continuous = true;
+        rec.interim = true;
+        rec.addEventListener("result", begin);
+    }
+
+function begin(event){
+    for(i = event.resultIndex; i < event.results.length; i++){
+        document.getElementById('number').innerHTML = event.results[i][0].transcript;
+    }
+}
+
+rec.start();
